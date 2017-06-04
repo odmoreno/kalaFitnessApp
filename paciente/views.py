c45baaa10dda336dcea0360ca8a8f950d8a8f8c1
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-06-04 12:54:42 -0500

    Un ejemplo de como funcionaria el template
    pongo un ejemplo de como funcionaria el template para mostrar los pacientes( solo para pruebas)

--- paciente/views.py
+++ paciente/views.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.db import transaction
 from django.shortcuts import render
+from paciente.models import Paciente
+from kalaapp.models import Usuario, Rol
+from django.contrib.auth.models import User 
 
 # Create your views here.
 def pacientes(request):
-    pass
+    template = 'paciente/pacientes.html'
+    p=Paciente.objects.all()
+    data = {
+            'pacientes':p,
+            
+        }
+    return render(request, template, data)
+
+@transaction.atomic
 def nuevoPaciente(request):
     pass
-def modificarPaciente(request):
+@transaction.atomic
+def modificarPaciente(request,paciente_id):
     pass
-def eliminarPaciente(request):
+@transaction.atomic
+def eliminarPaciente(request, paciente_id):
     pass
