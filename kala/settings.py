854b8e1b4dd9140b9bd063deb1a2119e37ce89e7
chaljara <chaljara@espol.edu.ec>
2017-06-19 03:32:49 -0500

    Srpint #2 task 74,75,76 plantillas facturas

--- kala/settings.py
+++ kala/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'kala.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestionhc',
-        'USER': 'b39a26ea8a2c83',
-        'PASSWORD': '49e6b5e4',
-        'HOST': 'us-cdbr-azure-southcentral-f.cloudapp.net',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    },
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'kaladb',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-    #     'PORT': '3307',
-    # }
+    #     'NAME': 'gestionhc',
+    #     'USER': 'b39a26ea8a2c83',
+    #     'PASSWORD': '49e6b5e4',
+    #     'HOST': 'us-cdbr-azure-southcentral-f.cloudapp.net',   # Or an IP Address that your DB is hosted on
+    #     'PORT': '3306',
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kalaapp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3307',
+    }
 }
 
 
