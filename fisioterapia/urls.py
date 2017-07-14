6f57120a548b47a5d0da16be4343db1c1fc216ad
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-07-14 09:45:26 -0500

    Sprint#3 Task 91, 93 (En Progreso)
    Implementacion preliminar de testeo de envios de mensaje

--- fisioterapia/urls.py
+++ fisioterapia/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'paciente'
+app_name = 'fisioterapia'
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^mensajes/$', views.verMensajes, name='mensajes'),
-    url(r'^mensajes/nuevo/$', views.nuevoMensaje, name='nuevoMensaje'),
-    url(r'^mensajes/leer/(?P<mensaje_id>[0-9]+)/$', views.leerMensaje, name='leerMensaje'),
+    url(r'^$', views.index, name="indexF"),
+    url(r'^mensajes/$', views.verMensajes, name='mensajesF'),
+    url(r'^mensajes/nuevo/$', views.nuevoMensaje, name='nuevoMensajeF'),
+    url(r'^mensajes/leer/(?P<mensaje_id>[0-9]+)/$', views.leerMensaje, name='leerMensajeF'),
 ]
