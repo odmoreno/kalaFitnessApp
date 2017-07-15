b0c104b4d12d6426fc69265638822359ec5dc8e2
odmoreno <oscarpol95@gmail.com>
2017-07-14 21:20:31 -0500

    Configuracion unitTest

--- personal/tests.py
+++ personal/tests.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from personal.models import Personal
+from kalaapp.models import Usuario, Rol
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+#class ingresarPersonalTest(TestCase):
+#    return
