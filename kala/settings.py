fc9187a2b3e569a646dd5f9958561a71343c4f11
chaljara <chaljara@espol.edu.ec>
2017-07-19 21:16:02 -0500

    Sprint#3 Task#105 106 Modelo rutina, html

--- kala/settings.py
+++ kala/settings.py
@@ -84,22 +84,22 @@ WSGI_APPLICATION = 'kala.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'gestionhc',
-         'USER': 'b39a26ea8a2c83',
-         'PASSWORD': '49e6b5e4',
-         'HOST': 'us-cdbr-azure-southcentral-f.cloudapp.net',   # Or an IP Address that your DB is hosted on
-         'PORT': '3306',
-     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'gestionhc',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-    #     'PORT': '3307',
-    # }
+     # 'default': {
+     #     'ENGINE': 'django.db.backends.mysql',
+     #     'NAME': 'gestionhc',
+     #     'USER': 'b39a26ea8a2c83',
+     #     'PASSWORD': '49e6b5e4',
+     #     'HOST': 'us-cdbr-azure-southcentral-f.cloudapp.net',   # Or an IP Address that your DB is hosted on
+     #     'PORT': '3306',
+     # },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gestionhc',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3307',
+    }
 }
 import sys
 if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
