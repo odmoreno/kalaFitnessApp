e25a3935f02210e4d6b1380c9430383db366afe0
Christian Jaramillo <chaljara@espol.edu.ec>
2017-07-21 04:15:41 -0500

    Sprint#3 Task#? Asignaciones de personal

--- kalaapp/urls.py
+++ kalaapp/urls.py
@@ -4,8 +4,6 @@ Created on Jun 4, 2017
 @author: carlos
 '''
 from django.conf.urls import url
-from kalaapp.views import index2
-from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'kalaapp'
@@ -13,4 +11,5 @@ app_name = 'kalaapp'
 urlpatterns = [
     #url(r'^$', index, name="index"),
     url(r'^$', views.index2 , name="index2"),
+    url(r'^asignar$', views.asignarPersonalaPaciente, name="AsignarPersonalaPaciente"),
 ]
