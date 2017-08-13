d8c75ea0299f159fde40f5cecdf7c34d2e0f72cd
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-08-13 15:36:10 -0500

    Sprint #5
    API Rest que lista ya los diagnosticos, personal, rutinas, dietas de un usuario especifico. Loggeo y auth BASICOS (Token auth no funcional aun)

--- clienteRest/urls.py
+++ clienteRest/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.authtoken import views
+
 from rest_framework.urlpatterns import format_suffix_patterns
 from clienteRest import views as api
 
@@ -7,6 +8,9 @@ from clienteRest import views as api
 urlpatterns = [
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^personal/', api.PersonalList.as_view()),
+    url(r'^diagnosticoN/', api.DiagnosticoNutList.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 
 ]
 urlpatterns= format_suffix_patterns(urlpatterns)
--- clienteRest/views.py
+++ clienteRest/views.py
@@ -14,9 +14,19 @@ from .serializers import PacienteSerializer, PersonalSerializer, UsuarioSerializ
 # Vistas que definiran la funcionalidad del API REST de cliente
 # Seguir Las Historias de Usuario de Cliente
 # Mi progreso, Mis Mensajes, Enviar Mensajes, Recibir Mensajes, Ver Citas, Separar Citas.
-class PersonalList(APIView):
 
+from rest_framework.authentication import BasicAuthentication
+
+
+class QuietBasicAuthentication(BasicAuthentication):
+    # disclaimer: once the user is logged in, this should NOT be used as a
+    def authenticate_header(self, request):
+        return 'xBasic realm="%s"' % self.www_authenticate_realm
+
+
+class PersonalList(APIView):
     def get(self, request):
+        print request.user
         personal=Personal.objects.all()
         usuarios=[]
         for x in personal:
@@ -24,70 +34,99 @@ class PersonalList(APIView):
         response=UsuarioSerializer(usuarios, many=True)
         return Response(response.data)
 
-    def post(self, request):
-        pass
-
+## Estas dos funciones retornan todos los diagnosticos en la base de datos
 class DiagnosticoNutList(APIView):
+
     def get(self, request, pacienteid=None):
-        paciente = get_object_or_404(Paciente, usuario=request.user)
-        diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
-        response = DiagnosticoNutSerializer(diagnosticos, many=True)
-        return Response(response.data or None)
+        try:
+            paciente = get_object_or_404(Paciente, usuario=request.user)
+            diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
+            response = DiagnosticoNutSerializer(diagnosticos, many=True)
+            return Response(response.data or None)
+        except:
+            return Response( {"data":"No es un paciente"})
 
 
 class DiagnosticoFisList(APIView):
+
     def get(self, request, pacienteid=None):
-        paciente =get_object_or_404(Paciente, usuario=request.user)
-        diagnosticos=DiagnosticoFisioterapia.objects.filter(paciente=paciente)
-        response=DiagnosticoFisSerializer(diagnosticos, many=True)
-        return Response(response.data or None)
+        try:
+            paciente =get_object_or_404(Paciente, usuario=request.user)
+            diagnosticos=DiagnosticoFisioterapia.objects.filter(paciente=paciente)
+            response=DiagnosticoFisSerializer(diagnosticos, many=True)
+            return Response(response.data or None)
 
+        except:
+            return Response({"data": "No es un paciente"})
+##############################################################################
 
+#Estas dos funciones retornas respectivamente todas las rutinas o Dietas que han sido asigndas a un paciente
 class RutinasList(APIView):
     def get(self, request):
-        paciente = get_object_or_404(Paciente, usuario=request.user)
-        diagnosticos = DiagnosticoFisioterapia.objects.filter(paciente=paciente)
-        rutinas=[]
-        for x in diagnosticos:
-            rut=x.rutina
-            for y in rut.subrutina:
-                rutinas.append(y)
-        response=SubrutinaSerializer(rutinas, many=True)
-        return Response(response.data or None)
+        try:
+            paciente = get_object_or_404(Paciente, usuario=request.user)
+            diagnosticos = DiagnosticoFisioterapia.objects.filter(paciente=paciente)
+            rutinas=[]
+            for x in diagnosticos:
+                rut=x.rutina
+                for y in rut.subrutina:
+                    rutinas.append(y)
+            response=SubrutinaSerializer(rutinas, many=True)
+            return Response(response.data or None)
+        except:
+            return Response({"data": "No es un paciente"})
 
 class DietasList(APIView):
+
     def get(self, request):
-        paciente = get_object_or_404(Paciente, usuario=request.user)
-        diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
-        dietas=[]
-        for x in diagnosticos:
-            dieta=x
-            plan=PlanNutDiario.objects.filter(dieta=dieta)
-            for y in plan:
-                dietas.append(y)
-        response=PlanNutDiarioSerializer(dietas, many=True)
-        return Response(response.data or None)
+        try:
+            paciente = get_object_or_404(Paciente, usuario=request.user)
+            diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
+            dietas=[]
+            for x in diagnosticos:
+                dieta=x
+                plan=PlanNutDiario.objects.filter(dieta=dieta)
+                for y in plan:
+                    dietas.append(y)
+            response=PlanNutDiarioSerializer(dietas, many=True)
+            return Response(response.data or None)
+        except:
+            return Response({"data": "No es un paciente"})
 
 
 class MensajesList(APIView):
 
+    #Para obtener los mensajes delPaciente
     def get(self, request):
+        pass
+    #Para enviar un mensaje
+    def post(self, request):
         pass
 
 
 
-def verMensajes(requets):
-    pass
 
-def nuevoMensaje(requets):
-    pass
 
-def miProgreso(request):
-    pass
+class HorariosList(APIView):
+    def get(self, request):
+        pass
+    def post(self, request, pacienteID, citaID):
+        pass
 
-def citasDisponibles(request):
-    pass
 
-def separarCita(request):
-    pass
 
+# def verMensajes(requets):
+#     pass
+#
+# def nuevoMensaje(requets):
+#     pass
+#
+# def miProgreso(request):
+#     pass
+#
+# def citasDisponibles(request):
+#     pass
+#
+# def separarCita(request):
+#     pass
+#
