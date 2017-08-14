b97cbd35e36ba6f3a026fa78f31235a9366640c6
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-08-14 00:29:53 -0500

    Sprint #5 Rest API

--- clienteRest/urls.py
+++ clienteRest/urls.py
@@ -8,8 +8,13 @@ from clienteRest import views as api
 urlpatterns = [
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^personal/', api.PersonalList.as_view()),
+    url(r'^personal/(?P<paciente_us>[0-9]+)/$', api.PersonalAsignadoList.as_view()),
     url(r'^diagnosticoN/(?P<paciente_us>[0-9]+)/$', api.DiagnosticoNutList.as_view()),
     url(r'^diagnosticoF/(?P<paciente_us>[0-9]+)/$', api.DiagnosticoFisList.as_view()),
+    url(r'^plandieta/(?P<paciente_us>[0-9]+)/$', api.DietasList.as_view()),
+    url(r'^rutinas/(?P<paciente_us>[0-9]+)/$', api.RutinasList.as_view()),
+    url(r'^fichas/fisioterapia/(?P<paciente_us>[0-9]+)/$', api.FichaFisList.as_view()),
+    url(r'^fichas/nutricion/(?P<paciente_us>[0-9]+)/$', api.FichaNutList.as_view()),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 
--- clienteRest/views.py
+++ clienteRest/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from kalaapp.models import Usuario
-from paciente.models import Paciente
+from paciente.models import Paciente, PacientePersonal
 from personal.models import Personal
 from diagnostico.models import DiagnosticoNutricion, DiagnosticoFisioterapia, Subrutina, PlanNutDiario, Rutina, Dieta
 from fisioterapia.models import Ficha
+
 from nutricion.models import ficha_nutricion
-from .serializers import PacienteSerializer, PersonalSerializer, UsuarioSerializer, DiagnosticoNutSerializer, DiagnosticoFisSerializer, RutinaSerializer, SubrutinaSerializer, DietaSerializer, PlanNutDiarioSerializer
+from .serializers import PacienteSerializer,FichaFisSerializer, FichaNutSerializer, PersonalSerializer, UsuarioSerializer, DiagnosticoNutSerializer, DiagnosticoFisSerializer, RutinaSerializer, SubrutinaSerializer, DietaSerializer, PlanNutDiarioSerializer
 # Vistas que definiran la funcionalidad del API REST de cliente
 # Seguir Las Historias de Usuario de Cliente
 # Mi progreso, Mis Mensajes, Enviar Mensajes, Recibir Mensajes, Ver Citas, Separar Citas.
@@ -27,11 +28,22 @@ class QuietBasicAuthentication(BasicAuthentication):
 class PersonalList(APIView):
     def get(self, request):
         print request.user
-        personal=Personal.objects.all()
+        personal=Personal.objects.filter(estado="A")
         usuarios=[]
         for x in personal:
-            usuarios.append(x.usuario)
+            if x.usuario.estado == "A":
+                usuarios.append(x.usuario)
         response=UsuarioSerializer(usuarios, many=True)
+        return Response(response.data)
+
+class PersonalAsignadoList(APIView):
+    def get(self, request, paciente_us):
+        print request.user
+        personal = PacientePersonal.objects.filter(personal_estado="A", paciente__usuario__cedula=paciente_us)
+        usuarios = []
+        for x in personal:
+            usuarios.append(x.personal.usuario)
+        response = UsuarioSerializer(usuarios, many=True)
         return Response(response.data)
 
 ## Estas dos funciones retornan todos los diagnosticos en la base de datos
@@ -42,7 +54,13 @@ class DiagnosticoNutList(APIView):
         try:
             paciente = get_object_or_404(Paciente, usuario__cedula=paciente_us)
             diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
+            dieta=[]
+            for d in diagnosticos:
+                dieta.append(d)
+            response2=DietaSerializer(dieta, many=True)
             response = DiagnosticoNutSerializer(diagnosticos, many=True)
+            print response2
+            print response
             return Response(response.data or None)
         except:
             return Response( {"data":"No es un paciente"})
@@ -64,28 +82,29 @@ class DiagnosticoFisList(APIView):
 #Estas dos funciones retornas respectivamente todas las rutinas o Dietas que han sido asigndas a un paciente
 class RutinasList(APIView):
     def get(self, request, paciente_us):
-        try:
-            paciente = get_object_or_404(Paciente, usuario=request.user)
-            diagnosticos = DiagnosticoFisioterapia.objects.filter(paciente=paciente)
-            rutinas=[]
-            for x in diagnosticos:
-                rut=x.rutina
-                for y in rut.subrutina:
-                    rutinas.append(y)
-            response=SubrutinaSerializer(rutinas, many=True)
-            return Response(response.data or None)
-        except:
-            return Response({"data": "No es un paciente"})
+        #try:
+        paciente = get_object_or_404(Paciente, usuario__cedula=paciente_us)
+        diagnosticos = DiagnosticoFisioterapia.objects.filter(paciente=paciente)
+        rutinas=[]
+        for x in diagnosticos:
+            rut=x.rutina
+            for y in rut.subrutina.all():
+                rutinas.append(y)
+        response=SubrutinaSerializer(rutinas, many=True)
+        return Response(response.data or {"mensaje":"No hay rutinas"})
+
+        #except:
+        #return Response({"mensaje": "No es un paciente"})
 
 class DietasList(APIView):
 
     def get(self, request, paciente_us):
         try:
-            paciente = get_object_or_404(Paciente, usuario=request.user)
+            paciente = get_object_or_404(Paciente, usuario__cedula=paciente_us)
             diagnosticos = DiagnosticoNutricion.objects.filter(paciente=paciente)
             dietas=[]
             for x in diagnosticos:
-                dieta=x
+                dieta=x.dieta
                 plan=PlanNutDiario.objects.filter(dieta=dieta)
                 for y in plan:
                     dietas.append(y)
@@ -94,6 +113,30 @@ class DietasList(APIView):
         except:
             return Response({"data": "No es un paciente"})
 
+class FichaFisList (APIView):
+    def get(self, request, paciente_us):
+        try:
+            paciente = get_object_or_404(Paciente, usuario__cedula=paciente_us)
+            ficha = Ficha.objects.filter(paciente=paciente)
+            fichas=[]
+            for x in ficha:
+                fichas.append(x)
+            response=FichaFisSerializer(fichas, many=True)
+            return Response(response.data or None)
+        except:
+            return Response({"data": "No es un paciente"})
+class FichaNutList (APIView):
+    def get(self, request, paciente_us):
+        try:
+            paciente = get_object_or_404(Paciente, usuario__cedula=paciente_us)
+            ficha = ficha_nutricion.objects.filter(paciente=paciente)
+            fichas=[]
+            for x in ficha:
+                fichas.append(x)
+            response=FichaNutSerializer(fichas, many=True)
+            return Response(response.data or None)
+        except:
+            return Response({"data": "No es un paciente"})
 
 class MensajesList(APIView):
 
